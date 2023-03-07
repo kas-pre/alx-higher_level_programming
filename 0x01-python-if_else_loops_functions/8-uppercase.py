40096971d446400c235c4d43fc9fbdb5a805abbf
Kasasira Precious <kasasirakp40@gmail.com>
2023-03-08 00:28:58 +0300

    Update 8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
-uppercase = __import__('8-uppercase').uppercase
-
-uppercase("holberton")
-uppercase("Holberton School 98 Battery street")
+def uppercase(str):
+    uppercase = 0
+    for i in str:
+        if(ord(i) >= 97 and ord(i) <= 122):
+            uppercase = 32
+        else:
+            uppercase = 0
+        print("{}".format(chr(ord(i) - uppercase)), end='')
+    print()
