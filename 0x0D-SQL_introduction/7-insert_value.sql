3cf3b3e32643ee201872b1f891947913fdd1f248
kas-pre <kasasirakp40@gmail.com>
2023-05-16 16:16:50 -0700

    Update solution
    Signed-off-by: kas-pre <kasasirakp40@gmail.com>

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,2 @@
 -- script inserts new row into table 'first_table' (db hbtn_0c_0) in MySQL server
-INSERT INTO first_table VALUES (89, 'Holberton School');
+INSERT INTO `first_table` (`id`, `name`) VALUES (89, 'Holberton School');
